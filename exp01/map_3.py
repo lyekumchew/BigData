8ea52c2d1a9936800b3e631914d2d23169277b19
lyekumchew <lyekumchew@gmail.com>
2020-04-05 18:38:33 +0800

    fix: mapper

--- exp01/map_3.py
+++ exp01/map_3.py
@@ -3,10 +3,7 @@ import sys
 
 def mapper():
     for line in sys.stdin:
-        k, v = line.strip().split(" ")  # 0_118 2
-        p1, p2 = k.split("_")  # 0_118
-        print("%s_%s %s" % (p1, p2, v))
-        # print("%s_%s %s" % (p2, p1, v))
+        print(line.strip())
 
 
 if __name__ == '__main__':
